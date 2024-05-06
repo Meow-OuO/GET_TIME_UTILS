4e17de00be8470082f93dfb80730f030511bafe1
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-05-06 20:57:51 +0800

    Delete js directory

--- js/Taipei_Time.js
+++
@@ -1,6 +0,0 @@
-setInterval(Time_now);
-
-function Time_now() {
-	let date = new Date();
-	document.getElementById("time").innerHTML = date.toLocaleString('sv-SE',{hourCycle:'h23',timeZone:'Asia/Taipei'});
-}
