6361db762111d62742d8941be80fcc2e32f41235
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-05-07 02:04:58 +0800

    Add files via upload

---
+++ Taipei/js/Taipei_Time.js
@@ -0,0 +1,10 @@
+window.setInterval(Time_now);
+
+function Time_now() {
+	let date = new Date();
+	document.getElementById("time").innerHTML = date.toLocaleString('en-ca',{weekday: 'short',year: 'numeric', month: 'numeric', day: 'numeric',hour: 'numeric', minute: 'numeric',second:'numeric',hourCycle:'h23',timeZone:'Asia/Taipei',timeZoneName: 'longOffset'})+'&ensp;'+'(台北標準時間)';
+}
+
+function TO_HOME() {
+window.location.href ='../index.html';
+}
